0fdc69bde3c1647379166cd1ec29ece56f39e28d
William Yaworsky <wj.px01@gmail.com>
2013-12-26 18:34:47 -0500

    Passing test case for getName

--- src/Lackey/Task/AbstractTask.php
+++ src/Lackey/Task/AbstractTask.php
@@ -2,9 +2,14 @@
 
 namespace Lackey\Task;
 
+use Doctrine\Common\Inflector\Inflector;
 use Lackey\Task;
 
 abstract class AbstractTask implements Task
 {
 
+    public function getName()
+    {
+        return str_replace('_', '-', Inflector::tableize(get_called_class()));
+    }
 }

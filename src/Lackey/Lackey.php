6d2d4199032732b7e3f2c08d24b45561aac3402b
William Yaworsky <wj.px01@gmail.com>
2013-12-26 18:08:52 -0500

    Added Task interface and building up Lackey API

---
+++ src/Lackey/Lackey.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Lackey;
+
+class Lackey
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public static function getInstance()
+    {
+        return new self();
+    }
+
+    public function loadComposerTask($name)
+    {
+
+    }
+}
---
+++ src/Lackey/Task.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Lackey;
+
+interface Task
+{
+
+}

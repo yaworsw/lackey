2494a65fde84fa87024294c0648976a29db976ce
William Yaworsky <wj.px01@gmail.com>
2013-12-29 17:38:07 -0500

    Added runAll to multitask

--- src/Lackey/MultiTask.php
+++ src/Lackey/MultiTask.php
@@ -5,4 +5,8 @@ namespace Lackey;
 abstract class MultiTask extends Task implements MultiTaskInterface
 {
 
+    final public function runAll(array $options = array())
+    {
+        return array_map($options, array($this, 'run'));
+    }
 }

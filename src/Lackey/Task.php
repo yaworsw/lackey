11b416067f24e1ece14b00fa57a41a3068d1c400
William Yaworsky <wj.px01@gmail.com>
2013-12-30 00:46:58 -0500

    Added asMultiClass static method to task

--- src/Lackey/Task.php
+++ src/Lackey/Task.php
@@ -7,6 +7,14 @@ use Lackey\Task;
 abstract class Task implements TaskInterface
 {
 
+    public static function asMultiTask()
+    {
+        $args    = func_get_args();
+        $reflect = new \ReflectionClass(get_called_class());
+        $task    = $reflect->newInstanceArgs($args);
+        return new Task\Multi($task);
+    }
+
     public function toMultiTask()
     {
         return new Task\Multi($this);
